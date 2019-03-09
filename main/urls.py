ab1955bb2d58c5c23090444fd73c272a75c45aad
sadra <sadrasafa@gmail.com>
2019-03-09 12:29:59 +0330

    link to scoring_detail and sentimentail_detail

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ app_name = "main"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^scoring_detail$', views.scoring_detail, name='scoring_detail'),
+    url(r'^sentimental_detail$', views.sentimental_detail, name='sentimental_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import redirect, render
 
 def index(request):
     return render(request, 'main/main.html')
+
+
+def scoring_detail(request):
+    return render(request, 'main/scoring_detail.html')
+
+
+def sentimental_detail(request):
+    return render(request, 'main/sentimental_detail.html')
